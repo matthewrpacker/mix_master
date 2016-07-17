6e3b886c4898885ac7aff3da403fed5c0115e0be
matthewrpacker <matthewrpacker@gmail.com>
2016-07-17 07:20:40 -0400

    Complete Iteration 5

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,10 @@
 class SongsController < ApplicationController
-
+before_action :set_artist, only: [:new, :create]
   def new
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.new
   end
 
   def create
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.create(song_params)
 
     redirect_to song_path(@song)
@@ -18,6 +16,9 @@ class SongsController < ApplicationController
 
 private
 
+def set_artist
+  @artist = Artist.find(params[:artist_id])
+end
   def song_params
     params.require(:song).permit(:title)
   end
